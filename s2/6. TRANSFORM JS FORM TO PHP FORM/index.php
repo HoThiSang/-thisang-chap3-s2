2c10b55bf3594e6e5927163c5fd99a563e2a118a
sang.ho <sang.ho25@student.passerellesnumeriques.org>
2023-12-02 18:53:47 +0700

    update index.php of 6

--- s2/6. TRANSFORM JS FORM TO PHP FORM/index.php
+++ s2/6. TRANSFORM JS FORM TO PHP FORM/index.php
@@ -9,14 +9,26 @@
 </head>
 
 <body>
-    <form action="js/script.php" method="post">
+    <form action="" method="post">
         <input type="password" name="password" id="password" placeholder="Password">
         <button type="submit" id="login_btn">Log in</button>
     </form>
 
-    <!-- <div id="hidden" style="display:none">This is sensitive data visible only if the user knows the correct password</div> -->
+    <div id="hidden" style="display:none">This is sensitive data visible only if the user knows the correct password
+    </div>
 
+    <?php
+    $password = isset($_POST['password']) ? $_POST['password'] : "";
+    // $password = isset($_POST['password'])? $_POST['password'] : "";
 
+    if ($password == "1234") {
+        echo "<script>
+                 document.getElementById('hidden').style.display = 'block';
+            </script>";
+        // echo "This is sensitive data visible only if the user knows the correct password";
+
+    }
+    ?>
 </body>
 
 </html>
